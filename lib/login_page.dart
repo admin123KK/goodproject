d5473e6ea3fe0cfcc1e48f8aad0f6970c10d87eb
unknown <karkiaku000@gmail.com>
2023-12-14 22:19:22 +0545

    frest maya

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -10,14 +10,76 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-
-appBar: AppBar(title: const Text('Pick Up Lines',style: TextStyle(color: Colors.white,),),
-backgroundColor: Colors.pink,
-centerTitle: true,
-),
-backgroundColor: Colors.white,
-body: Center(child: Text('We are Nepali'),),
-    );
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            'Pick Up Lines',
+            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+          ),
+          backgroundColor: Colors.pink,
+          centerTitle: true,
+        ),
+        body: Padding(
+          padding: EdgeInsets.all(9),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                const Column(
+                  children: [
+                    Text('Welcome'),
+                  ],
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  children: [
+                    Container(
+                      height: 250,
+                      width: 250,
+                      color: Colors.red,
+                    )
+                  ],
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  children: [
+                    Container(
+                      height: 250,
+                      width: 250,
+                      color: Colors.red,
+                    )
+                  ],
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  children: [
+                    Container(
+                      height: 250,
+                      width: 250,
+                      color: Colors.red,
+                    )
+                  ],
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  children: [
+                    Container(
+                      height: 250,
+                      width: 250,
+                      color: Colors.red,
+                    )
+                  ],
+                )
+              ],
+            ),
+          ),
+        ));
   }
 }
