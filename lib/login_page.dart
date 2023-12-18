6d6c97ecf63eb6884c56d8ccf5a93d30c3fb94a7
unknown <karkiaku000@gmail.com>
2023-12-18 20:39:27 +0545

    nicel done decoration box radius

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -11,87 +11,105 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text(
-            'Pick Up Lines',
-            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
-          ),
-          backgroundColor: Colors.pink,
-          centerTitle: true,
+      appBar: AppBar(
+        title: const Text(
+          'Pick Up Lines',
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
         ),
-        body: Padding(
-          padding: const EdgeInsets.all(15),
-          child: SingleChildScrollView(
-            child: Column(
-              children: [
-                const Column(
-                  children: [
-                    Text(
-                      'Welcome',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                      ),
+        backgroundColor: Colors.pink,
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(15),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              const Column(
+                children: [
+                  Text(
+                    'Welcome',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
                     ),
-                  ],
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Container(
-                      height: 250,
-                      width: 300,
-                      color: Colors.red,
-                      child: const Text('Hello Namste Nepal'),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    height: 250,
+                    width: 300,
+                    decoration: BoxDecoration(
+                      color: Colors.green,
+                      borderRadius: BorderRadius.circular(24),
                     ),
-                  ],
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Container(
-                      height: 250,
-                      width: 300,
-                      color: Colors.red,
-                    )
-                  ],
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Container(
-                      height: 250,
-                      width: 300,
-                      color: Colors.red,
-                    )
-                  ],
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Container(
-                      height: 250,
-                      width: 300,
-                      color: Colors.red,
-                    )
-                  ],
-                )
-              ],
-            ),
+                    child: const Center(child: Text('Hello Namste Nepal')),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    height: 250,
+                    width: 300,
+                    decoration: BoxDecoration(
+                        color: Colors.green,
+                        borderRadius: BorderRadius.circular(24)),
+                    child: const Center(
+                      child: Text('Hello Namste Nepal'),
+                    ),
+                  )
+                ],
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    height: 250,
+                    width: 300,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(24),
+                      color: Colors.green,
+                    ),
+                    child: const Center(
+                      child: Text('Hello Namste Nepal'),
+                    ),
+                  )
+                ],
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    height: 250,
+                    width: 300,
+                    decoration: BoxDecoration(
+                      color: Colors.green,
+                      borderRadius: BorderRadius.circular(24),
+                    ),
+                    child: const Center(child: Text('Hello Namste Nepal')),
+                  )
+                ],
+              )
+            ],
           ),
         ),
-        drawer: Drawer(),
-        );
+      ),
+      drawer: Drawer(),
+    );
   }
 }
