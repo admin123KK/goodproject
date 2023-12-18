1093a35e3d7c3d31e11a41de478c5f5d56768542
unknown <karkiaku000@gmail.com>
2023-12-18 21:09:48 +0545

    success image in container

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -11,12 +11,13 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey.shade300,
       appBar: AppBar(
         title: const Text(
           'Pick Up Lines',
           style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
         ),
-        backgroundColor: Colors.pink,
+        backgroundColor: Color.fromARGB(255, 181, 7, 7),
         centerTitle: true,
       ),
       body: Padding(
@@ -42,12 +43,16 @@ class _LoginPageState extends State<LoginPage> {
                 children: [
                   Container(
                     height: 250,
-                    width: 300,
+                    width: 350,
                     decoration: BoxDecoration(
+                    image: DecorationImage(image: NetworkImage('https://images.unsplash.com/photo-1702750722257-6bc38db1267a?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'),fit: BoxFit.cover,),
                       color: Colors.green,
                       borderRadius: BorderRadius.circular(24),
                     ),
-                    child: const Center(child: Text('Hello Namste Nepal')),
+                
+                    child: const Center(child: Text('"Hello Namste Nepal"',style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,fontSize: 25),),
+                    ),
+                  
                   ),
                 ],
               ),
@@ -59,12 +64,13 @@ class _LoginPageState extends State<LoginPage> {
                 children: [
                   Container(
                     height: 250,
-                    width: 300,
+                    width: 350,
                     decoration: BoxDecoration(
+                      image: DecorationImage(image: NetworkImage('https://images.unsplash.com/photo-1702594369985-163331c12097?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'),fit: BoxFit.cover),
                         color: Colors.green,
                         borderRadius: BorderRadius.circular(24)),
                     child: const Center(
-                      child: Text('Hello Namste Nepal'),
+                      child: Text('"Hello Namste Nepal"',style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,fontSize: 25),),
                     ),
                   )
                 ],
@@ -77,13 +83,14 @@ class _LoginPageState extends State<LoginPage> {
                 children: [
                   Container(
                     height: 250,
-                    width: 300,
+                    width: 350,
                     decoration: BoxDecoration(
+                      image: DecorationImage(image: NetworkImage('https://images.unsplash.com/photo-1682687218147-9806132dc697?q=80&w=1975&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'),fit: BoxFit.cover),
                       borderRadius: BorderRadius.circular(24),
                       color: Colors.green,
                     ),
                     child: const Center(
-                      child: Text('Hello Namste Nepal'),
+                      child: Text('"Hello Namste Nepal"',style: TextStyle(fontWeight: FontWeight.bold,color: Colors.white,fontSize: 25),),
                     ),
                   )
                 ],
@@ -96,12 +103,13 @@ class _LoginPageState extends State<LoginPage> {
                 children: [
                   Container(
                     height: 250,
-                    width: 300,
+                    width: 350,
                     decoration: BoxDecoration(
+                      image: DecorationImage(image: NetworkImage('https://images.unsplash.com/photo-1611516491426-03025e6043c8?q=80&w=1933&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'),fit: BoxFit.cover),
                       color: Colors.green,
                       borderRadius: BorderRadius.circular(24),
                     ),
-                    child: const Center(child: Text('Hello Namste Nepal')),
+                    child: const Center(child: Text('"Hello Namste Nepal"',style: TextStyle(fontWeight: FontWeight.bold,color: Colors.white,fontSize: 25),)),
                   )
                 ],
               )
