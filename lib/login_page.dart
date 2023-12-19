88b183244ab004a59d68802e52c04d9e1bc5e4ae
unknown <karkiaku000@gmail.com>
2023-12-19 20:46:01 +0545

    sucess drawer

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -45,14 +45,23 @@ class _LoginPageState extends State<LoginPage> {
                     height: 250,
                     width: 350,
                     decoration: BoxDecoration(
-                    image: DecorationImage(image: NetworkImage('https://images.unsplash.com/photo-1702750722257-6bc38db1267a?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'),fit: BoxFit.cover,),
+                      image: const DecorationImage(
+                        image: NetworkImage(
+                            'https://images.unsplash.com/photo-1702750722257-6bc38db1267a?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'),
+                        fit: BoxFit.cover,
+                      ),
                       color: Colors.green,
                       borderRadius: BorderRadius.circular(24),
                     ),
-                
-                    child: const Center(child: Text('"Hello Namste Nepal"',style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,fontSize: 25),),
+                    child: const Center(
+                      child: Text(
+                        '"Hello Namste Nepal"',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 25),
+                      ),
                     ),
-                  
                   ),
                 ],
               ),
@@ -66,11 +75,20 @@ class _LoginPageState extends State<LoginPage> {
                     height: 250,
                     width: 350,
                     decoration: BoxDecoration(
-                      image: DecorationImage(image: NetworkImage('https://images.unsplash.com/photo-1702594369985-163331c12097?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'),fit: BoxFit.cover),
+                        image: const DecorationImage(
+                            image: NetworkImage(
+                                'https://images.unsplash.com/photo-1702594369985-163331c12097?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'),
+                            fit: BoxFit.cover),
                         color: Colors.green,
                         borderRadius: BorderRadius.circular(24)),
                     child: const Center(
-                      child: Text('"Hello Namste Nepal"',style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,fontSize: 25),),
+                      child: Text(
+                        '"Hello Namste Nepal"',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 25),
+                      ),
                     ),
                   )
                 ],
@@ -85,12 +103,21 @@ class _LoginPageState extends State<LoginPage> {
                     height: 250,
                     width: 350,
                     decoration: BoxDecoration(
-                      image: DecorationImage(image: NetworkImage('https://images.unsplash.com/photo-1682687218147-9806132dc697?q=80&w=1975&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'),fit: BoxFit.cover),
+                      image: const DecorationImage(
+                          image: NetworkImage(
+                              'https://images.unsplash.com/photo-1682687218147-9806132dc697?q=80&w=1975&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'),
+                          fit: BoxFit.cover),
                       borderRadius: BorderRadius.circular(24),
                       color: Colors.green,
                     ),
                     child: const Center(
-                      child: Text('"Hello Namste Nepal"',style: TextStyle(fontWeight: FontWeight.bold,color: Colors.white,fontSize: 25),),
+                      child: Text(
+                        '"Hello Namste Nepal"',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                            fontSize: 25),
+                      ),
                     ),
                   )
                 ],
@@ -105,11 +132,21 @@ class _LoginPageState extends State<LoginPage> {
                     height: 250,
                     width: 350,
                     decoration: BoxDecoration(
-                      image: DecorationImage(image: NetworkImage('https://images.unsplash.com/photo-1611516491426-03025e6043c8?q=80&w=1933&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'),fit: BoxFit.cover),
+                      image: const  DecorationImage(
+                          image: NetworkImage(
+                              'https://images.unsplash.com/photo-1611516491426-03025e6043c8?q=80&w=1933&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'),
+                          fit: BoxFit.cover),
                       color: Colors.green,
                       borderRadius: BorderRadius.circular(24),
                     ),
-                    child: const Center(child: Text('"Hello Namste Nepal"',style: TextStyle(fontWeight: FontWeight.bold,color: Colors.white,fontSize: 25),)),
+                    child: const Center(
+                        child: Text(
+                      '"Hello Namste Nepal"',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                          fontSize: 25),
+                    )),
                   )
                 ],
               )
@@ -117,7 +154,22 @@ class _LoginPageState extends State<LoginPage> {
           ),
         ),
       ),
-      drawer: Drawer(),
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            UserAccountsDrawerHeader(
+              accountName: const Text('Aakash Karki'),
+              accountEmail: const Text('Skyisgood666@gmail.com'),
+              currentAccountPicture: CircleAvatar(
+                child: ClipOval(
+                  child: Image.asset('image/assets/ak.jpg',fit: BoxFit.cover,),
+                ),
+              ),
+        
+            )
+          ],
+        ),
+      ),
     );
   }
 }
