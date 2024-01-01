538704e0b66e7622739c17f00036173c82781fcf
unknown <karkiaku000@gmail.com>
2024-01-01 10:18:48 +0545

    working hard on download

--- lib/share_page.dart
+++ lib/share_page.dart
@@ -3,6 +3,7 @@ import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 import 'package:image_gallery_saver/image_gallery_saver.dart';
 import 'package:share_plus/share_plus.dart';
+
 class SharePage extends StatelessWidget {
   final String imageUrl =
       'https://images.unsplash.com/photo-1682687218147-9806132dc697?q=80&w=1975&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D';
@@ -12,7 +13,7 @@ class SharePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Image Viewer'),
+        title: Text('Image Viewers is good in '),
       ),
       body: Stack(
         children: [
@@ -63,37 +64,39 @@ class SharePage extends StatelessWidget {
       ),
     );
   }
-void _onDownloadPressed(BuildContext context, String imageUrl) async {
-  try {
-    var response = await http.get(Uri.parse(imageUrl));
-    Uint8List bytes = response.bodyBytes;
 
-    var result = await ImageGallerySaver.saveImage(Uint8List.fromList(bytes));
-    
-    if (result != null && result.isNotEmpty) {
-      print('Image saved: $result');
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(
-          content: Text('Image downloaded successfully'),
-        ),
-      );
-    } else {
-      print('Error saving image');
+  void _onDownloadPressed(BuildContext context, String imageUrl) async {
+    try {
+      var response = await http.get(Uri.parse(imageUrl));
+      Uint8List bytes = response.bodyBytes;
+
+      var result = await ImageGallerySaver.saveImage(Uint8List.fromList(bytes));
+
+      if (result != null && result.isNotEmpty) {
+        print('Image saved: $result');
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text('Image downloaded successfully'),
+          ),
+        );
+      } else {
+        print('Error saving image');
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text('Error downloading image'),
+          ),
+        );
+      }
+    } catch (e) {
+      print('Error saving image: $e');
       ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(
           content: Text('Error downloading image'),
         ),
       );
     }
-  } catch (e) {
-    print('Error saving image: $e');
-    ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(
-        content: Text('Error downloading image'),
-      ),
-    );
   }
-}
+
   void _onSharePressed(BuildContext context, String imageUrl) {
     Share.share('Check out this image: $imageUrl');
   }
