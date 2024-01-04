1132973d3b9a2ddeb21531bab46cec65aad18a04
unknown <karkiaku000@gmail.com>
2024-01-04 09:04:18 +0545

    Signup Page 20% success

--- lib/signup_page.dart
+++ lib/signup_page.dart
@@ -1,10 +1,45 @@
 import 'package:flutter/material.dart';
 
-class SignUp extends StatelessWidget {
+class SignUp extends StatefulWidget {
   const SignUp({super.key});
 
   @override
+  State<SignUp> createState() => _SignUpState();
+}
+
+class _SignUpState extends State<SignUp> {
+  @override
   Widget build(BuildContext context) {
-    return  Scaffold(backgroundColor: Colors.green,);
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: SafeArea(
+          child: Column(
+            children: [
+              Container(
+                alignment: Alignment.topLeft,
+                child: const Text(
+                  'Create Account ',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 0),
+                child: Container(
+                  alignment: Alignment.topLeft,
+                  child: const Text(
+                    'Enter your details to signup',
+                    style: TextStyle(fontSize: 12),
+                  ),
+                ),
+              ),
+              Container(
+                child: Image.asset('image/assets/signup.png'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
