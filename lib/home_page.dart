aa2c1e1405520864e3b7ebc0aeb7d3d443121245
unknown <karkiaku000@gmail.com>
2024-01-13 19:25:43 +0545

    nice in the alert of the logout

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:goodproject/login_page.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -9,6 +11,51 @@ class HomePage extends StatefulWidget {
 
 class _LoginPageState extends State<HomePage> {
   String imageUrl = '';
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  Future<void> _showLogOUtDialog(BuildContext, context) {
+    return showDialog(
+        context: context,
+        builder: (BuildContext) {
+          return AlertDialog(
+            title: const Text(
+              'Logout',
+              style: TextStyle(fontFamily: 'Mooli'),
+            ),
+            content: const  Text(
+              'Are you sure want to logout?',
+              style: TextStyle(color: Colors.red, fontFamily: 'Mooli'),
+            ),
+            actions: <Widget>[
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: const Text(
+                  'Cancel',
+                  style: TextStyle(
+                    color: Color(0xFF91AD13),
+                  ),
+                ),
+              ),
+              TextButton(
+                  onPressed: () {
+                    Navigator.pushAndRemoveUntil(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => LoginPage(),
+                        ),
+                        (route) => false);
+                  },
+                  child: const Text(
+                    'Logout',
+                    style: TextStyle(color: Colors.red),
+                  ))
+            ],
+          );
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -236,6 +283,20 @@ class _LoginPageState extends State<HomePage> {
                 Icons.sports_cricket_rounded,
               ),
               title: Text('Score Card'),
+            ),
+            ListTile(
+              leading: const Icon(
+                Icons.logout,
+                color: Colors.red,
+              ),
+              title: const Text(
+                'Logout',
+                style:
+                    TextStyle(color: Colors.red, fontWeight: FontWeight.bold),
+              ),
+              onTap: () {
+                _showLogOUtDialog(BuildContext, context);
+              },
             )
           ],
         ),
