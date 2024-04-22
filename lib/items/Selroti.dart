1a0ca8a63c54ace7fb4eabb61c1c38ff29a3dbac
unknown <karkiaku000@gmail.com>
2024-04-23 00:20:21 +0545

    details items prog

--- lib/items/Selroti.dart
+++ lib/items/Selroti.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 
 class SelRotiPage extends StatefulWidget {
   const SelRotiPage({super.key});
@@ -13,100 +13,140 @@ class _SelRotiPageState extends State<SelRotiPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          Image.asset(
-            'assets/images/selroti.png',
-            fit: BoxFit.cover,
+      backgroundColor: Colors.white,
+      body: Stack(children: [
+        Image.asset(
+          'assets/images/selroti.png',
+          fit: BoxFit.cover,
+        ),
+        Container(
+          height: 400,
+          width: 450,
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 32, 24, 24),
+                Colors.transparent,
+                Color.fromARGB(255, 37, 28, 28)
+              ],
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+            ),
           ),
-          Container(
-            height: 400,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 33),
+              child: IconButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                icon: const Icon(
+                  Icons.arrow_back,
+                ),
+                color: Colors.white,
+              ),
+            ),
+            Row(
+              children: [
+                IconButton(
+                  onPressed: () {},
+                  icon: const Icon(
+                    Icons.shopping_cart_checkout,
+                    color: Color(0xFF91AD13),
+                    size: 29,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+        Padding(
+          padding: EdgeInsets.symmetric(vertical: 380),
+          child: Container(
+            height: 300,
             width: 450,
             decoration: const BoxDecoration(
-              gradient: LinearGradient(
-                colors: [
-                  Color.fromARGB(255, 32, 24, 24),
-                  Colors.transparent,
-                  Color.fromARGB(255, 37, 28, 28)
-                ],
-                begin: Alignment.topCenter,
-                end: Alignment.bottomCenter,
-              ),
-            ),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 33),
-                child: IconButton(
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
-                  icon: const Icon(
-                    Icons.arrow_back,
-                  ),
-                  color: Colors.white,
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(30),
+                    topRight: Radius.circular(30))),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  height: 20,
                 ),
-              ),
-              Row(
-                children: [
-                  IconButton(
-                    onPressed: () {},
-                    icon: const Icon(
-                      Icons.shopping_cart_checkout,
-                      color: Color(0xFF91AD13),
-                      size: 29,
-                    ),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 30),
+                  child: Text(
+                    'SelRoti',
+                    style: TextStyle(
+                        fontFamily: 'Mooli',
+                        fontWeight: FontWeight.bold,
+                        fontSize: 29),
                   ),
-                ],
-              ),
-            ],
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 380),
-            child: Container(
-              height: 300,
-              width: 450,
-              decoration: const BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(30),
-                      topRight: Radius.circular(30))),
-              child: Padding(
-                padding: EdgeInsets.all(11.0),
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      'SelRoti',
-                      style: TextStyle(
-                          fontSize: 30,
-                          fontFamily: 'Mooli',
-                          fontWeight: FontWeight.bold),
-                    ),
-                  Column(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        Text('Rs.60',
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 22),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          IgnorePointer(
+                            ignoring: false,
+                            child: RatingBar.builder(
+                              initialRating: 3,
+                              minRating: 1,
+                              direction: Axis.horizontal,
+                              allowHalfRating: true,
+                              itemCount: 5,
+                              itemSize: 20,
+                              itemPadding:
+                                  EdgeInsets.symmetric(horizontal: 1.0),
+                              itemBuilder: (context, _) => Icon(
+                                Icons.star,
+                                color: Colors.orange[600],
+                              ),
+                              onRatingUpdate: (rating) {
+                                print(rating);
+                              },
+                            ),
+                          ),
+                          Text(
+                            '  3 Star',
+                            style: TextStyle(color: Colors.orange[600]),
+                          ),
+                        ],
+                      ),
+                      const Column(
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
+                          Text(
+                            'Rs.60',
                             style: TextStyle(
-                                fontSize: 20,
                                 color: Colors.green,
-                                fontWeight: FontWeight.bold)),
-                        // SizedBox(
-                        //   height: 2,
-                        // ),
-                        Text('/per piece')
-                      ],
-                    ),
-                  ],
+                                fontSize: 25,
+                                fontWeight: FontWeight.bold),
+                          ),
+                          SizedBox(
+                            height: 1,
+                          ),
+                          Text('/per piece')
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
-              ),
+              ],
             ),
           ),
-        ],
-      ),
+        ),
+      ]),
     );
   }
 }
