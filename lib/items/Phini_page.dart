14eea7538f0e3c5c413848f7836c4a1d73d471a0
unknown <karkiaku000@gmail.com>
2024-04-23 19:46:34 +0545

    phini item

--- lib/items/Phini_page.dart
+++ lib/items/Phini_page.dart
@@ -1,15 +1,126 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 
-class PhiniPage extends StatelessWidget {
+class PhiniPage extends StatefulWidget {
   const PhiniPage({super.key});
 
   @override
+  State<PhiniPage> createState() => _PhiniPageState();
+}
+
+class _PhiniPageState extends State<PhiniPage> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        height: 400,
-        width: 300,
-        child: Image.asset('assets/images/phini.png'),
+      body: Stack(
+        children: [
+          Image.asset(
+            'assets/images/phini.png',
+            fit: BoxFit.cover,
+          ),
+          Container(
+            height: 340,
+            width: 450,
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Color.fromARGB(255, 32, 24, 24),
+                  Colors.transparent,
+                  Color.fromARGB(255, 37, 28, 28)
+                ],
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+              ),
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 33),
+                child: IconButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  icon: Icon(
+                    Icons.arrow_back,
+                  ),
+                  color: Colors.white,
+                ),
+              ),
+              Row(
+                children: [
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(Icons.shopping_cart_checkout_outlined),
+                    color: Colors.white,
+                  ),
+                ],
+              ),
+            ],
+          ),
+          SafeArea(
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 270),
+                  child: Container(
+                    height: 300,
+                    width: 440,
+                    decoration: const BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(30),
+                          topRight: Radius.circular(30)),
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                       const  Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 30),
+                          child: Text(
+                            'Phini',
+                            style: TextStyle(
+                                fontFamily: 'Mooli',
+                                fontWeight: FontWeight.bold,
+                                fontSize: 37),
+                          ),
+                        ),
+                        Column(
+                          children: [
+                            Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: 25),
+                              child: RatingBar.builder(
+                                initialRating: 4,
+                                minRating: 1,
+                                direction: Axis.horizontal,
+                                allowHalfRating: true,
+                                itemCount: 5,
+                                itemSize: 17,
+                                itemPadding:
+                                    const EdgeInsets.symmetric(horizontal: 1.0),
+                                itemBuilder: (context, _) => Icon(
+                                  Icons.star,
+                                  color: Colors.orange[600],
+                                ),
+                                onRatingUpdate: (rating) {
+                                  print(rating);
+                                },
+                              ),
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
