149dc7ea93de47176fd1eb1fb922fe070918b7af
unknown <karkiaku000@gmail.com>
2024-05-08 18:38:03 +0545

    cart success on frontend

--- lib/items/cart.dart
+++ lib/items/cart.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class CartPage extends StatefulWidget {
@@ -8,7 +11,24 @@ class CartPage extends StatefulWidget {
 }
 
 class _CartPageState extends State<CartPage> {
+  String userName = "";
+
   @override
+  void initState() {
+    super.initState();
+    getCurrentUser();
+  }
+
+  void getCurrentUser() {
+    FirebaseAuth auth = FirebaseAuth.instance;
+    User? user = auth.currentUser;
+    if (user != null) {
+      setState(() {
+        userName = user.displayName ?? "";
+      });
+    }
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
@@ -55,6 +75,18 @@ class _CartPageState extends State<CartPage> {
               ],
             ),
           ),
+          Center(
+              child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Your Cart is Empty $userName',
+                style:
+                    const TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+              ),
+              // Text(userName),
+            ],
+          )),
           Column(
             mainAxisAlignment: MainAxisAlignment.end,
             children: [
@@ -66,6 +98,164 @@ class _CartPageState extends State<CartPage> {
                   borderRadius: BorderRadius.only(
                     topLeft: Radius.circular(27),
                     topRight: Radius.circular(27),
+                  ),
+                ),
+                child: SingleChildScrollView(
+                  child: Padding(
+                    padding: const EdgeInsets.only(
+                      bottom: 35,
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const Padding(
+                          padding: EdgeInsets.all(8.0),
+                          child: Text(
+                            'Total amounts',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 50),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: [
+                              InkWell(
+                                onTap: () {
+                                  showDialog(
+                                      context: context,
+                                      builder: (BuildContext context) {
+                                        return const Center(
+                                            child: CircularProgressIndicator(
+                                                color: Color(0xFF91AD13)));
+                                      });
+                                  Timer(Duration(seconds: 2), () {
+                                    Navigator.pop(context);
+                                    showDialog(
+                                        context: context,
+                                        builder: (BuildContext context) {
+                                          return AlertDialog(
+                                            title: const Text(
+                                              'Payement Status',
+                                              style: TextStyle(
+                                                  fontFamily: 'Mooli',
+                                                  fontWeight: FontWeight.bold,
+                                                  fontSize: 27),
+                                            ),
+                                            content: Column(
+                                              mainAxisSize: MainAxisSize.min,
+                                              children: [
+                                                const Text(
+                                                  'Total amounts: Rs.500',
+                                                  style: TextStyle(
+                                                    fontWeight: FontWeight.bold,
+                                                    fontSize: 20,
+                                                    color: Color.fromARGB(
+                                                        255, 28, 139, 31),
+                                                  ),
+                                                ),
+                                                const SizedBox(
+                                                  height: 10,
+                                                ),
+                                                const Text('Pay with'),
+                                                Divider(
+                                                  thickness: 0.5,
+                                                  color: Colors.grey[600],
+                                                ),
+                                                Row(
+                                                  mainAxisAlignment:
+                                                      MainAxisAlignment.center,
+                                                  children: [
+                                                    GestureDetector(
+                                                      child: ClipOval(
+                                                        child: Image.asset(
+                                                          'assets/images/esewa.png',
+                                                          width: 45,
+                                                          height: 45,
+                                                        ),
+                                                      ),
+                                                    ),
+                                                    const SizedBox(
+                                                      width: 20,
+                                                    ),
+                                                    GestureDetector(
+                                                      child: ClipOval(
+                                                        child: Image.asset(
+                                                          'assets/images/khalti.png',
+                                                          height: 65,
+                                                          width: 65,
+                                                        ),
+                                                      ),
+                                                    ),
+                                                  ],
+                                                ),
+                                                const Text('OR'),
+                                              ],
+                                            ),
+                                            actions: <Widget>[
+                                              TextButton(
+                                                onPressed: () {
+                                                  Navigator.pop(context);
+                                                },
+                                                child: const Text(
+                                                  'Confirm Cash on Delivery',
+                                                  style: TextStyle(
+                                                    fontWeight: FontWeight.bold,
+                                                    color: Color.fromARGB(
+                                                        255, 28, 139, 31),
+                                                  ),
+                                                ),
+                                              ),
+                                              TextButton(
+                                                onPressed: () {
+                                                  Navigator.pop(context);
+                                                },
+                                                child: const Text(
+                                                  'Cancel',
+                                                  style: TextStyle(
+                                                      color: Colors.black),
+                                                ),
+                                              ),
+                                            ],
+                                          );
+                                        });
+                                  });
+                                },
+                                child: Container(
+                                  height: 40,
+                                  width: 110,
+                                  decoration: BoxDecoration(
+                                    color: Colors.black,
+                                    borderRadius: BorderRadius.circular(27),
+                                  ),
+                                  child: const Center(
+                                    child: Text(
+                                      'Order Now',
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          color: Colors.white),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        const Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 40),
+                          child: Text(
+                            'Rs.500',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20,
+                                fontFamily: "Mooli"),
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               ),
